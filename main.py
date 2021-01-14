249c96de8fbc54170ea6952976e5ef5f9d50e1ae
samuelowad <samuelowolabi330@gmail.com>
2021-01-14 19:48:22 +0100

    project started

---
+++ main.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+# cat_img_path = '/home/blackspider/Documents/pythonpro/tensorFlow/image/cat.jpeg'
+
+# cat_img = Image.open(cat_img_path)
+# cat_img.show()
+
+dis_ima_path = input('Enter image path: ')
+dis_ima = Image.open(dis_ima_path)
+dis_ima.show()
